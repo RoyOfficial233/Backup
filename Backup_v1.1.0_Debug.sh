b73ab9fe7644ce03b48d3b2fd51fd74fe5a386f3
RoyDong0908 <sunmooneasy@outlook.com>
2025-07-28 15:11:23 +0800

    Updated v1.1.0

--- Backup_v1.1.0_Debug.sh
+++ Backup_v1.1.0_Debug.sh
@@ -9,6 +9,7 @@ P1="/opt/mcsmanager"
 P2="/www/survival.server"
 P3="/root/.ossutilconfig"
 P4="/www/mod.server"
-zip -vr $File $P1 $P2 $P3 $P4 > $Compress_Log
-ossutil --loglevel=debug --log-file=$Upload_Log -c /root/.ossutilconfig cp $File "oss://roy-baks/Backups/" -f
-ossutil -c /root/.ossutilconfig cp $Upload_Log "oss://roy-baks/Backups/" -fq
+zip -vr "${File}" "${P1}" "${P2}" "${P3}" "${P4}" > "${Compress_Log}"
+ossutil --loglevel=debug --log-file="${Upload_Log}" -c /root/.ossutilconfig cp "${File}" "oss://roy-baks/Backups/" -f
+ossutil -c /root/.ossutilconfig cp "${Upload_Log}" "oss://roy-baks/Backups/" -fq
+ossutil -c /root/.ossutilconfig cp "${Compress_Log}" "oss://roy-baks/Backups/" -fq
