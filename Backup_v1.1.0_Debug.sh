516d8ebfa1ec080678994be94bd896149afb6cc7
RoyOfficial233 <royofficial@royofficial.com>
2025-08-27 13:07:50 +0800

    upload /Backup_v1.1.0_Debug.sh

--- Backup_v1.1.0_Debug.sh
+++ Backup_v1.1.0_Debug.sh
@@ -8,3 +8,4 @@ P2="/www/survival.server"
 P3="/root/.ossutilconfig"
 P4="/www/mod.server"
 zip -vr $File $P1 $P2 $P3 $P4 > $Compress_Log
+ossutil --ignore-existing -f -c /root/.ossutilconfig sync "/www/Backups/" "oss://roy-baks/Backups/"
